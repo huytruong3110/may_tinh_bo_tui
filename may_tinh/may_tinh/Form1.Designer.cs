ba71d352408731075bea39da871c5201ad254afa
huytruong3110 <127746949+huytruong3110@users.noreply.github.com>
2024-03-13 02:04:30 -0700

    Add files via upload

---
+++ may_tinh/may_tinh/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace may_tinh
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listView1 = new ListView();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button9 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button8 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button10 = new Button();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(25, 18);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(628, 409);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(41, 108);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(41, 41);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(593, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(41, 164);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 3;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(41, 215);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 4;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(186, 108);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 5;
+            button4.Text = "5";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(186, 164);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 29);
+            button5.TabIndex = 6;
+            button5.Text = "6";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(186, 215);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 29);
+            button6.TabIndex = 7;
+            button6.Text = "7";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(329, 108);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 29);
+            button7.TabIndex = 8;
+            button7.Text = "9";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(329, 164);
+            button9.Name = "button9";
+            button9.Size = new Size(94, 29);
+            button9.TabIndex = 9;
+            button9.Text = "0";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(41, 265);
+            button11.Name = "button11";
+            button11.Size = new Size(94, 29);
+            button11.TabIndex = 11;
+            button11.Text = "4";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(186, 265);
+            button12.Name = "button12";
+            button12.Size = new Size(94, 29);
+            button12.TabIndex = 12;
+            button12.Text = "8";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.ActiveCaption;
+            button8.Location = new Point(464, 108);
+            button8.Name = "button8";
+            button8.Size = new Size(67, 85);
+            button8.TabIndex = 14;
+            button8.Text = "+";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click_1;
+            // 
+            // button14
+            // 
+            button14.BackColor = SystemColors.ActiveCaption;
+            button14.Location = new Point(567, 108);
+            button14.Name = "button14";
+            button14.Size = new Size(67, 79);
+            button14.TabIndex = 15;
+            button14.Text = "-";
+            button14.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            button15.BackColor = SystemColors.ActiveCaption;
+            button15.Location = new Point(464, 215);
+            button15.Name = "button15";
+            button15.Size = new Size(67, 79);
+            button15.TabIndex = 16;
+            button15.Text = "*";
+            button15.UseVisualStyleBackColor = false;
+            // 
+            // button16
+            // 
+            button16.BackColor = SystemColors.ActiveCaption;
+            button16.Location = new Point(567, 215);
+            button16.Name = "button16";
+            button16.Size = new Size(67, 83);
+            button16.TabIndex = 17;
+            button16.Text = "/";
+            button16.UseVisualStyleBackColor = false;
+            // 
+            // button17
+            // 
+            button17.BackColor = SystemColors.ActiveCaption;
+            button17.Location = new Point(329, 215);
+            button17.Name = "button17";
+            button17.Size = new Size(94, 29);
+            button17.TabIndex = 18;
+            button17.Text = "AC";
+            button17.UseVisualStyleBackColor = false;
+            button17.Click += button17_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = SystemColors.ActiveCaption;
+            button10.Location = new Point(329, 265);
+            button10.Name = "button10";
+            button10.Size = new Size(94, 29);
+            button10.TabIndex = 19;
+            button10.Text = "=";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button10);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button8);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button9);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(listView1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private Button button1;
+        private TextBox textBox1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button9;
+        private Button button11;
+        private Button button12;
+        private Button button8;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button10;
+    }
+}
---
+++ may_tinh/may_tinh/Form1.cs
@@ -0,0 +1,259 @@
+namespace may_tinh
+{
+    public partial class Form1 : Form
+    {
+        private void handle_operator_click(object sender, EventArgs e)
+        { 
+        Button button = sender as Button;
+            string operator_Value = button.Text;
+            if (is_choosen)
+            {
+                after_string = after_string + button.Text;
+            }
+            else { 
+            before_string = before_string + button.Text;
+            textBox1.Text = before_string;
+            }
+        }
+        private void ResetValue()
+        {
+            is_choosen = false;
+            operator_value = " ";
+            after_string = " ";
+            before_string= " ";
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += button10_Click;
+            button2.Click += button10_Click;
+            button3.Click += button10_Click;
+            button4.Click += button10_Click;
+            button5.Click += button10_Click;
+            button6.Click += button10_Click;
+            button7.Click += button10_Click;
+            button8.Click += button10_Click;
+            button9.Click += button10_Click;
+        }
+        string before_string = " ";
+        string after_string = " ";
+        string operator_value = " ";
+        bool is_choosen = false;
+        float result;
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button1 = sender as Button;
+            string buttonText = button1.Text;
+            if (is_choosen)
+            {
+                textBox1.Text = "";
+                after_string = after_string + button1.Text;
+
+            }
+            else
+            {
+                before_string = before_string + buttonText;
+                textBox1.Text = before_string;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button button2 = sender as Button;
+            string buttonText = button2.Text;
+            if (is_choosen)
+            {
+                textBox1.Text = "";
+                after_string = after_string + button2.Text;
+            }
+            else
+            {
+                before_string = before_string + buttonText;
+                textBox1.Text = before_string;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button button3 = sender as Button;
+            string buttonText = button3.Text;
+            if (is_choosen)
+            {
+                textBox1.Text = "";
+                after_string = after_string + button3.Text;
+            }
+            else
+            {
+                before_string = before_string + buttonText;
+                textBox1.Text = before_string;
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Button button11 = sender as Button;
+            string buttonText = button11.Text;
+            if (is_choosen)
+            {
+                textBox1.Text = "";
+                after_string = after_string + button11.Text;
+            }
+            else
+            {
+                before_string = before_string + buttonText;
+                textBox1.Text = before_string;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button button4 = sender as Button;
+            string buttonText = button4.Text;
+            if (is_choosen)
+            {
+                textBox1.Text = "";
+                after_string = after_string + button4.Text;
+            }
+            else
+            {
+                before_string = before_string + buttonText;
+                textBox1.Text = before_string;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button button5 = sender as Button;
+            string buttonText = button5.Text;
+            if (is_choosen)
+            {
+                textBox1.Text = "";
+                after_string = after_string + button5.Text;
+            }
+            else
+            {
+                before_string = before_string + buttonText;
+                textBox1.Text = before_string;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button button6 = sender as Button;
+            string buttonText = button6.Text;
+            if (is_choosen)
+            {
+                textBox1.Text = "";
+                after_string = after_string + button6.Text;
+            }
+            else
+            {
+                before_string = before_string + buttonText;
+                textBox1.Text = before_string;
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Button button12 = sender as Button;
+            string buttonText = button12.Text;
+            if (is_choosen)
+            {
+                textBox1.Text = "";
+                after_string = after_string + button12.Text;
+            }
+            else
+            {
+                before_string = before_string + buttonText;
+                textBox1.Text = before_string;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button button7 = sender as Button;
+            string buttonText = button7.Text;
+            if (is_choosen)
+            {
+                textBox1.Text = "";
+                after_string = after_string + button7.Text;
+            }
+            else
+            {
+                before_string = before_string + buttonText;
+                textBox1.Text = before_string;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button button9 = sender as Button;
+            string buttonText = button9.Text;
+            if (is_choosen)
+            {
+                textBox1.Text = "";
+                after_string = after_string + button9.Text;
+            }
+            else
+            {
+                before_string = before_string + buttonText;
+                textBox1.Text = before_string;
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            int number1 = int.Parse(before_string);
+            int number2 = int.Parse(after_string);
+            int result;
+            switch(operator_value)
+            {
+                case "+":
+                    {
+                        result = number1 + number2;
+                        break;
+                    }
+                    default: 
+                    {
+                        result = 0; break;
+                    }
+                case "-":
+                    {
+                        result = number1 - number2;
+                        break;
+                    }
+               
+                case "*":
+                    {
+                        result = number1 * number2;
+                        break;
+                    }
+                
+                case "/":
+                    {
+                        result = number1 * number2;
+                        break;
+                    }
+
+
+            }
+            textBox1.Text += number1;
+            ResetValue();
+        }
+    }
+}
